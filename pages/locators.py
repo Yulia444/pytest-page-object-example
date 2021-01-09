4e9ace05309ca4510d9a7fa79821b7b938598342
julia <olkhovyk_ak16@nuwm.edu.ua>
2021-01-09 19:16:08 +0200

    correct code style pages/locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,17 +1,21 @@
 from selenium.webdriver.common.by import By
 
+
 class BasePageLocators():
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     USER_ICON = (By.CSS_SELECTOR, "i.icon-user")
+
 
 class BasketPageLocators():
     BASKET_LINK = (By.XPATH, '//a[@class="btn btn-default"]')
     BASKET_IS_EMPTY = (By.XPATH, '//p[contains(text(),"Your basket is empty")]')
     BASKET_LIST = (By.CSS_SELECTOR, 'form.basket_summary')
 
+
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '.login_form')
@@ -20,6 +24,7 @@ class LoginPageLocators():
     REGISTER_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password1')
     REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password2')
     REGISTER_BUTTON = (By.TAG_NAME, 'button[name="registration_submit"]')
+
 
 class ProductPageLocators():
     ADD_TO_CART = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
