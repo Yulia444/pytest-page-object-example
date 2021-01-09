7ecceed457fe7e9e0b7b2e437976031ae03394dd
julia <olkhovyk_ak16@nuwm.edu.ua>
2021-01-09 19:39:45 +0200

    correct code style in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,12 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
 
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="Choose language example: es")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
@@ -14,7 +14,9 @@ def browser(request):
     if user_language:
         print("\nstart chrome browser for test..")
         options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        options.add_experimental_option('prefs', 
+                                        {'intl.accept_languages': 
+                                            user_language})
         browser = webdriver.Chrome(options=options)
     else:
         raise pytest.UsageError("--language is required param")
