2b99b8d9aa95d9f43d8a27e0fe79c7a2e009f2bf
julia <olkhovyk_ak16@nuwm.edu.ua>
2021-01-09 19:28:42 +0200

    correct code style login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
 from .locators import LoginPageLocators
+
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -27,7 +27,7 @@ class LoginPage(BasePage):
             send_keys(email)
         self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD).\
             send_keys(password)
-        self.browser.find_element(*LoginPageLocators.REGISTER_CONFIRM_PASSWORD).\
+        self.browser.find_element(*LoginPageLocators.REGISTER_CONF_PASSWORD).\
             send_keys(password)
         self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
         
